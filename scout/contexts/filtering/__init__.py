f2c86acb261a4ea6e30960b62f45fd6939d92203
Sean Stafford <smstafford.physics@gmail.com>
2025-10-13 18:34:31 -0400

    fix: added newer functions to the init import in the filtering context (forgot to before)

--- scout/contexts/filtering/__init__.py
+++ scout/contexts/filtering/__init__.py
@@ -7,6 +7,8 @@ Handles filtering and analysis of scraped job listings based on various criteria
 from scout.contexts.filtering.filters import (
     check_active,
     check_red_flags,
+    check_clearance_req,
+    check_title_red_flags,
 )
 
 __all__ = [
